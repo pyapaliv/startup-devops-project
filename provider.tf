170c431a850cc8bbe9e2b9cb223d633f67ec6821
pyapaliv <133226535+pyapaliv@users.noreply.github.com>
2023-05-12 09:59:58 -0400

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 # This block sets the AWS provider configuration for Terraform. 
-# In this case, we're using the "aws" provider and setting the region to "eu-west-2".
+# In this case, we're using the "aws" provider and setting the region to "us-east-1".
 
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
   shared_credentials_files = ["/home/ogasege/.aws/credentials"]
 }
 
